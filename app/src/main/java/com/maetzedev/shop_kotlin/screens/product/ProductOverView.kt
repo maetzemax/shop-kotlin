575d2e949b3780de25b0c8bd733916a13a7f8c23
mmaetze <maetze35@gmail.com>
2022-05-31 18:24:28 +0200

    updated product overview ui

--- app/src/main/java/com/maetzedev/shop_kotlin/screens/product/ProductOverView.kt
+++ app/src/main/java/com/maetzedev/shop_kotlin/screens/product/ProductOverView.kt
@@ -2,85 +2,143 @@ package com.maetzedev.shop_kotlin.screens.product
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale.Companion.FillWidth
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Timestamp
 import com.maetzedev.shop_kotlin.R
 import com.maetzedev.shop_kotlin.models.product.Product
+import com.maetzedev.shop_kotlin.screens.destinations.HomeScreenDestination
 import com.maetzedev.shop_kotlin.ui.theme.ShopkotlinTheme
 import com.maetzedev.shop_kotlin.uicomponents.component.BottomBar
-import com.maetzedev.shop_kotlin.uicomponents.component.TopBar
 import com.maetzedev.shop_kotlin.utils.Formatters
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
 @Destination
 @Composable
 fun ProductOverView(
-    product: Product
+    product: Product,
+    navigator: DestinationsNavigator
 ) {
 
     val currencyFormatter = Formatters.CurrencyFormatter()
 
     Scaffold(
-        topBar = { TopBar(title = product.name) },
         bottomBar = { BottomBar() }
     ) {
         Column(
-            Modifier.padding(horizontal = 20.dp)
+            Modifier
+                .padding(horizontal = 20.dp)
+                .padding(top = 40.dp)
         ) {
-            Image(
-                painterResource(id = R.drawable.car_placeholder),
-                contentDescription = null,
-                contentScale = FillWidth,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-
             LazyColumn(
-                modifier = Modifier.padding(it),
                 content = {
-                item {
-                    Row {
-                        Text(
-                            product.name,
-                            style = MaterialTheme.typography.h4
+                    item {
+
+                        Icon(
+                            Icons.Rounded.ArrowBack,
+                            "BACK",
+                            Modifier.clickable { navigator.navigate(HomeScreenDestination()) }
                         )
 
-                        Spacer(
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+
+                            Text(
+                                product.name,
+                                style = MaterialTheme.typography.h4,
+                                modifier = Modifier.width(200.dp)
+                            )
+
+                            Spacer(modifier = Modifier.weight(1f))
+
+                            Text(
+                                currencyFormatter.formatFloatToString(product.price),
+                                modifier = Modifier
+                                    .background(Color(0xFF243156))
+                                    .padding(horizontal = 5.dp),
+                                color = Color.White,
+                                fontWeight = FontWeight.ExtraBold,
+                                style = MaterialTheme.typography.h6
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Image(
+                            painterResource(id = R.drawable.car_placeholder),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
                             modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 10.dp)
+                                .fillMaxWidth()
                         )
+
+                        Row {
+                            Spacer(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(horizontal = 10.dp)
+                            )
+
+                        }
+
+                        Spacer(Modifier.height(10.dp))
 
                         Text(
-                            currencyFormatter.formatFloatToString(product.price),
-                            style = MaterialTheme.typography.h4
+                            text = "Beschreibung",
+                            style = MaterialTheme.typography.h5,
+                            fontWeight = FontWeight.SemiBold
                         )
-                    }
 
-                    Text("Verkäufer: ${product.seller}")
+                        Spacer(Modifier.height(10.dp))
 
-                    Spacer(Modifier.height(20.dp))
+                        Text(product.description)
 
-                    Text(product.description)
-                    
-                    Spacer(Modifier.height(50.dp))
-                    
-                    Button(onClick = { /*TODO: Add to Shopping Cart*/ }) {
-                        Text(text = "Zum Warenkorb hinzufügen")
+                        Spacer(Modifier.height(30.dp))
+
+                        Row {
+                            Spacer(Modifier.weight(1f))
+
+                            Text(
+                                modifier = Modifier
+                                    .padding(bottom = it.calculateBottomPadding() + 20.dp)
+                                    .background(
+                                        Color(
+                                            0xFF4160B4
+                                        )
+                                    )
+                                    .clickable { /*TODO: Add to Shopping Cart*/ }
+                                    .padding(10.dp),
+                                text = "ZUM WARENKORB HINZUFÜGEN",
+                                fontWeight = FontWeight.ExtraBold,
+                                color = Color.White
+                            )
+
+                            Spacer(Modifier.weight(1f))
+
+                        }
+
                     }
                 }
-            }
             )
         }
     }
@@ -109,7 +167,8 @@ fun ProductOverView_Preview() {
                 name = "testname",
                 seller = "Maetzi",
                 created = Timestamp.now()
-            )
+            ),
+            navigator = EmptyDestinationsNavigator
         )
     }
 }
