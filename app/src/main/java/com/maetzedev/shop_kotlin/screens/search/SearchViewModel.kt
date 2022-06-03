bfb326d98ed944ae8bffb6467c65dfdd3b639c0a
Niclas Frohberg <niclas-frohberg@t-online.de>
2022-06-04 00:46:48 +0200

    add Search

--- app/src/main/java/com/maetzedev/shop_kotlin/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/maetzedev/shop_kotlin/screens/search/SearchViewModel.kt
@@ -1,23 +1,19 @@
-package com.maetzedev.shop_kotlin.screens.home
+package com.maetzedev.shop_kotlin.screens.search
 
-
-import com.google.firebase.FirebaseApp
 import com.maetzedev.shop_kotlin.models.product.Product
 import com.maetzedev.shop_kotlin.repositories.ProductsRepo
 import com.maetzedev.shop_kotlin.repositories.UserRepo
 
-class HomeScreenViewModel {
-
-    private val productRepository by lazy { ProductsRepo() }
+class SearchViewModel {
+    private val productsRepo by lazy { ProductsRepo() }
     private val userRepo by lazy { UserRepo() }
 
-    init {
-        FirebaseApp.initializeApp(FirebaseApp.getInstance().applicationContext)
+    val products = productsRepo.fetchProducts()
+    val userData = userRepo.fetchUserData()
+
+    fun filterByQuery(query: String, products: List<Product?>): List<Product?> {
+        return products.filter { it!!.name.contains(query) }
     }
-
-    var currentListState = this.productRepository.fetchProducts()
-    var userData = this.userRepo.fetchUserData()
-
 
     fun mapProducts(products: List<Product?>, likedProducts: List<Int>, productsCart: List<Int>): List<Product?> {
         val mappedProducts: MutableList<Product?> = emptyList<Product>().toMutableList()
